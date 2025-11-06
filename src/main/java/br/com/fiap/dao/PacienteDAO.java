9b275d8db30304e31c5b8cd9c223662e3aadf265
ryanvetoriano <vetorianosilva@gmail.com>
2025-11-05 23:41:58 -0300

    bugfix: Fechando as sessões corretamente no DAO de paciente, para não dar erro se limite de sessão.

--- src/main/java/br/com/fiap/dao/PacienteDAO.java
+++ src/main/java/br/com/fiap/dao/PacienteDAO.java
@@ -3,110 +3,105 @@ package br.com.fiap.dao;
 import br.com.fiap.beans.Paciente;
 import br.com.fiap.conexoes.ConexaoFactory;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class PacienteDAO {
 
-    public Connection minhaConexao;
-
-    public PacienteDAO() throws SQLException, ClassNotFoundException {
-        this.minhaConexao = new ConexaoFactory().conexao();
-    }
+    // 🔹 Não manter a conexão como atributo de instância
 
     // insert
-    public String insert(Paciente paciente) throws SQLException {
-        PreparedStatement stmt =
-                minhaConexao.prepareStatement("INSERT INTO PACIENTE (NOME_PACIENTE, CPF_PACIENTE, DATA_NASCIMENTO, TELEFONE_PACIENTE, SEXO_PACIENTE) VALUES (?, ?, ?, ?, ?)");
-                stmt.setString(1,paciente.getNome());
-                stmt.setString(2,paciente.getCpf());
-                stmt.setDate(3,java.sql.Date.valueOf(paciente.getDataNascimento()));
-                stmt.setString(4,paciente.getTelefone());
-                stmt.setString(5,paciente.getSexo());
+    public String insert(Paciente paciente) throws SQLException, ClassNotFoundException {
+        String sql = "INSERT INTO PACIENTE (NOME_PACIENTE, CPF_PACIENTE, DATA_NASCIMENTO, TELEFONE_PACIENTE, SEXO_PACIENTE) VALUES (?, ?, ?, ?, ?)";
+        try (Connection conn = new ConexaoFactory().conexao();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
 
-                stmt.execute();
-                stmt.close();
+            stmt.setString(1, paciente.getNome());
+            stmt.setString(2, paciente.getCpf());
+            stmt.setDate(3, java.sql.Date.valueOf(paciente.getDataNascimento()));
+            stmt.setString(4, paciente.getTelefone());
+            stmt.setString(5, paciente.getSexo());
 
-                return "Paciente cadastrado com sucesso!";
+            stmt.executeUpdate();
+            return "Paciente cadastrado com sucesso!";
+        }
     }
 
-    public String delete(String cpf) throws SQLException {
-        PreparedStatement stmt =
-                minhaConexao.prepareStatement("Delete from PACIENTE where CPF_PACIENTE = ?");
-        stmt.setString(1, cpf);
+    // delete
+    public String delete(String cpf) throws SQLException, ClassNotFoundException {
+        String sql = "DELETE FROM PACIENTE WHERE CPF_PACIENTE = ?";
+        try (Connection conn = new ConexaoFactory().conexao();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
 
-        stmt.execute();
-        stmt.close();
-
-        return "Paciente deletado com sucesso!";
+            stmt.setString(1, cpf);
+            stmt.executeUpdate();
+            return "Paciente deletado com sucesso!";
+        }
     }
 
-    // UpDate
-    public String update (Paciente paciente) throws SQLException {
-        PreparedStatement stmt =
-                minhaConexao.prepareStatement("Update PACIENTE set NOME_PACIENTE = ?, DATA_NASCIMENTO = ?, TELEFONE_PACIENTE = ?, SEXO_PACIENTE = ? WHERE CPF_PACIENTE = ?");
-        stmt.setString(1, paciente.getNome());
-        stmt.setDate(2, java.sql.Date.valueOf(paciente.getDataNascimento()));
-        stmt.setString(3, paciente.getTelefone());
-        stmt.setString(4, paciente.getSexo());
-        stmt.setString(5, paciente.getCpf());
+    // update
+    public String update(Paciente paciente) throws SQLException, ClassNotFoundException {
+        String sql = "UPDATE PACIENTE SET NOME_PACIENTE = ?, DATA_NASCIMENTO = ?, TELEFONE_PACIENTE = ?, SEXO_PACIENTE = ? WHERE CPF_PACIENTE = ?";
+        try (Connection conn = new ConexaoFactory().conexao();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
 
-        stmt.executeUpdate();
-        stmt.close();
+            stmt.setString(1, paciente.getNome());
+            stmt.setDate(2, java.sql.Date.valueOf(paciente.getDataNascimento()));
+            stmt.setString(3, paciente.getTelefone());
+            stmt.setString(4, paciente.getSexo());
+            stmt.setString(5, paciente.getCpf());
 
-        return "Paciente atualizado com sucesso!";
+            stmt.executeUpdate();
+            return "Paciente atualizado com sucesso!";
+        }
     }
 
-    // Select
-    public List<Paciente> select() throws SQLException {
-        List<Paciente> listPacientes = new ArrayList<>();
+    // select all
+    public List<Paciente> select() throws SQLException, ClassNotFoundException {
+        List<Paciente> pacientes = new ArrayList<>();
+        String sql = "SELECT * FROM PACIENTE";
 
-        PreparedStatement stmt = minhaConexao.prepareStatement("SELECT * FROM PACIENTE");
-        ResultSet rs = stmt.executeQuery();
+        try (Connection conn = new ConexaoFactory().conexao();
+             PreparedStatement stmt = conn.prepareStatement(sql);
+             ResultSet rs = stmt.executeQuery()) {
 
-        while (rs.next()) {
-            Paciente paciente = new Paciente();
-            paciente.setIdPaciente(rs.getInt("ID_PACIENTE"));
-            paciente.setNome(rs.getString("NOME_PACIENTE"));
-            paciente.setCpf(rs.getString("CPF_PACIENTE"));
-            paciente.setDataNascimento(rs.getDate("DATA_NASCIMENTO").toLocalDate());
-            paciente.setTelefone(rs.getString("TELEFONE_PACIENTE"));
-            paciente.setSexo(rs.getString("SEXO_PACIENTE"));
-
-            listPacientes.add(paciente);
+            while (rs.next()) {
+                Paciente p = new Paciente();
+                p.setIdPaciente(rs.getInt("ID_PACIENTE"));
+                p.setNome(rs.getString("NOME_PACIENTE"));
+                p.setCpf(rs.getString("CPF_PACIENTE"));
+                p.setDataNascimento(rs.getDate("DATA_NASCIMENTO").toLocalDate());
+                p.setTelefone(rs.getString("TELEFONE_PACIENTE"));
+                p.setSexo(rs.getString("SEXO_PACIENTE"));
+                pacientes.add(p);
+            }
         }
 
-        rs.close();
-        stmt.close();
-
-        return listPacientes;
+        return pacientes;
     }
 
-    public Paciente buscarLogin(String cpf) throws SQLException {
+    // buscar por CPF (login)
+    public Paciente buscarLogin(String cpf) throws SQLException, ClassNotFoundException {
         Paciente paciente = null;
-
         String sql = "SELECT * FROM PACIENTE WHERE CPF_PACIENTE = ?";
-        PreparedStatement stmt = minhaConexao.prepareStatement(sql);
-        stmt.setString(1, cpf);
 
-        ResultSet rs = stmt.executeQuery();
+        try (Connection conn = new ConexaoFactory().conexao();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
 
-        if (rs.next()) {
-            paciente = new Paciente(rs.getInt("ID_PACIENTE"), rs.getString("NOME_PACIENTE"));
-            paciente.setIdPaciente(rs.getInt("ID_PACIENTE"));
-            paciente.setNome(rs.getString("NOME_PACIENTE"));
-            paciente.setCpf(rs.getString("CPF_PACIENTE"));
-            paciente.setDataNascimento(rs.getDate("DATA_NASCIMENTO").toLocalDate());
-            paciente.setTelefone(rs.getString("TELEFONE_PACIENTE"));
-            paciente.setSexo(rs.getString("SEXO_PACIENTE"));
+            stmt.setString(1, cpf);
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    paciente = new Paciente();
+                    paciente.setIdPaciente(rs.getInt("ID_PACIENTE"));
+                    paciente.setNome(rs.getString("NOME_PACIENTE"));
+                    paciente.setCpf(rs.getString("CPF_PACIENTE"));
+                    paciente.setDataNascimento(rs.getDate("DATA_NASCIMENTO").toLocalDate());
+                    paciente.setTelefone(rs.getString("TELEFONE_PACIENTE"));
+                    paciente.setSexo(rs.getString("SEXO_PACIENTE"));
+                }
+            }
         }
-
-        rs.close();
-        stmt.close();
 
         return paciente;
     }
